8c5c84699c3c9145c083b79677792f19698c892c
Adharshini10 <lingampallyadharshini@gmail.com>
2026-06-13 22:50:02 +0530

    Day1

---
+++ Day1.py
@@ -0,0 +1,35 @@
+# Day 1: Hello Python
+# Starting with the absolute basics
+
+# Printing text
+print("Hello, World!")   # print() is used to display the text
+
+# Printing numbers
+print(2026)              #2026
+
+# Printing multiple values
+print("My age is", 20) # Mixing text + numbers
+
+# Variables
+name = "Vaishu"
+age = 20
+score = 84
+
+print("Name:", name)  # Name: Vaishu
+print("Age:", age)
+print("Score:", score)
+
+# Simple math operations
+print("2 + 3 =", 2 + 3)  # 2 + 3 = 5
+print("10 - 4 =", 10 - 4)
+print("5 * 6 =", 5 * 6)
+print("20 / 5 =", 20 / 5)
+
+# Comments are notes for the reader
+# They don’t affect the code
+
+# How to run this file:
+# Save it as Day1.py
+# Open terminal/command prompt
+# Navigate to the folder where the file is saved
+# Run: python Day1.py
